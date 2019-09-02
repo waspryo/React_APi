aeea38c1a4842b4777323f8e8d5d2c3cc7a556a0
Ryo Sugimoto <wasp2123321@gmail.com>
2019-09-02 16:28:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react';
-
-// function App() {
-
-//   constructor(prpps) {
-//     super(props);
-//     this.state = {
-//       items: [],
-//       isLoaded: false,
-//     }
-//   }
-
-//   componemtDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(res => res.json())
-//     .then(json => {
-//       this.setState({
-//         isLoaded: true,
-//         items: json,
-//       })
-//     })
-//   }
-
-//   return (
-
-//     var { isLoaded, items } = this.state;
-
-//     if (!isLoaded) {
-//       return <div>Loading...</div>;
-//     }
-
-//     else {
-
-//     <div className="App">
-//       Data has been loaded
-//     </div>
-//   );
-// }
-
-// export default App;
-=======
 import React from 'react';
 
 function App() {
@@ -79,5 +37,3 @@ function App() {
   );
 }
 
-export default App;
->>>>>>> 5f55c12278a62e9746658eb8f700c90451563928
